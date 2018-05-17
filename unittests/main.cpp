ca5bc72e109f5661af7a03a8d53fd3af17803056
William Li <libcxx@nym.hush.com>
2018-05-17 07:37:47 -0700

    Simplify unittests/main.cpp

--- unittests/main.cpp
+++ unittests/main.cpp
@@ -23,15 +23,6 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include <gtest/gtest.h>
 
-using ::testing::EmptyTestEventListener;
-using ::testing::InitGoogleTest;
-using ::testing::Test;
-using ::testing::TestCase;
-using ::testing::TestEventListeners;
-using ::testing::TestInfo;
-using ::testing::TestPartResult;
-using ::testing::UnitTest;
-
 int
 main(int argc, char** argv)
 {
