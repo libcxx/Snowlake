e2c1a052d5efa614e86894ca1e404c1b22ec8115
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-08-27 06:45:48 +0000

    Try this

--- src/ArgumentParser.cpp
+++ src/ArgumentParser.cpp
@@ -131,8 +131,9 @@ ArgumentParser::addParameter(const char* name, const char short_hand,
   CmdlOption opts{.short_hand = short_hand,
                   .description = description,
                   .required = required,
+                  .default_value = {default_val},
+                  .value = {default_val},
                   .dst = reinterpret_cast<void*>(res),
-                  .default_value = {default_val}
                 };
   m_shorthand_map[short_hand] = name;
   m_opts[name] = opts;
