f9093849410645889675a696018f0b266ce7f7f2
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-08-27 06:55:19 +0000

    Fix this

--- src/ArgumentParser.cpp
+++ src/ArgumentParser.cpp
@@ -132,7 +132,7 @@ ArgumentParser::addParameter(const char* name, const char short_hand,
                   .description = description,
                   .required = required,
                   .default_value = {default_val},
-                  .value = {default_val},
+                  .value = CmdlOptionValue(),
                   .dst = reinterpret_cast<void*>(res),
                 };
   m_shorthand_map[short_hand] = name;
