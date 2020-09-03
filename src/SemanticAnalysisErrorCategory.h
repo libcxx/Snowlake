28e40697a433b3447e948f5d5bf47ebe000efd1b
Tomiko <554685+tetrachrome@users.noreply.github.com>
2020-09-03 16:20:18 +0000

    Add missing #include in src/SemanticAnalysisErrorCategory.h

--- src/SemanticAnalysisErrorCategory.h
+++ src/SemanticAnalysisErrorCategory.h
@@ -24,6 +24,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #pragma once
 
 #include "CompilerErrorCategory.h"
+#include "SemanticAnalysisErrorCodes.h"
 
 #include <cassert>
 
